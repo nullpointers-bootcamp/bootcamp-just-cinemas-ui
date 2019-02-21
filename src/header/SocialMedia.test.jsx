8c62759f46c2b6e54f9fc74840cbcbdfb7ee3c8b
muhilvarnan <muhilvarnan.v@gmail.com>
2019-02-21 13:32:30 +0530

    header test case refractor

--- src/header/SocialMedia.test.jsx
+++ src/header/SocialMedia.test.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { BrowserRouter as Router } from "react-router-dom";
 import SocialMedia from "./SocialMedia";
 
 it("show Social media", () => {
   const tree = renderer
     .create(
-      <Router>
-        <SocialMedia href="https://www.facebook.com/ThoughtWorks/" src="https://s3.ap-south-1.amazonaws.com/twspicinemas/assets/fb_icon.gif" />
-      </Router>
+      <SocialMedia
+        href="https://www.facebook.com/ThoughtWorks/"
+        src="https://s3.ap-south-1.amazonaws.com/twspicinemas/assets/fb_icon.gif"
+      />
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
