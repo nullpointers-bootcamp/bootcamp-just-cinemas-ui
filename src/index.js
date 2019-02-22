d0c20a7e5aa6a1aabf61fc5dfc8d63fcbd0dc63f
muhilvarnan <muhilvarnan.v@gmail.com>
2019-02-22 10:08:22 +0530

    set page title

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.css';
-import App from './app/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
+import "./index.css";
+import App from "./app/App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+document.title = "Just Cinemas";
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
